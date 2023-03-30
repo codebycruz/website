4b823ce3d063d262e20b078a7eda879ad1d26a68
DvvCz <86097860+DvvCz@users.noreply.github.com>
2023-03-30 15:55:32 -0700

    Change backend url
    Backend is now at the back.codebycruz.com subdomain

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -2,7 +2,7 @@ import PocketBase from "pocketbase";
 
 import { writable } from "svelte/store";
 
-export const pb = new PocketBase("https://codebycruz.com");
+export const pb = new PocketBase("https://back.codebycruz.com");
 
 export const currentUser = writable(pb.authStore.model);
 
