aa778c52a2715c84e0f99055ecf5d7335668ae3f
David Cruz <david@codebycruz.com>
2025-09-28 14:32:36 -0700

    Contributions effect in background

---
+++ src/data/contributions.ts
@@ -0,0 +1,17 @@
+/**
+ * Returns github contributions for the last year.
+ */
+export async function getGithubContributions() {
+	return (
+		await fetch(
+			"https://github-contributions-api.jogruber.de/v4/dvvcz?y=last",
+		)
+	).json() as Promise<{
+		total: Record<string, number>;
+		contributions: {
+			date: string;
+			count: number;
+			level: 0 | 1 | 2 | 3 | 4;
+		}[];
+	}>;
+}
